0e14f64dd06fda07a6df2f8d10b498a7415893ee
S0M3N <thesomen123@gmail.com>
2022-08-17 08:24:47 +0530

    first commit

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+#urls for the apps here
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    s = """
+    <h1 style='color: green'>Hello World!</h1>
+    <p>lorem ipsum</p>
+    <hr/>
+    
+    """
+    return HttpResponse(s)
