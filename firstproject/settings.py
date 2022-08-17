a65a037a6597d42abe94aa16628a9c863b24dbfc
S0M3N <thesomen123@gmail.com>
2022-08-17 08:43:54 +0530

    setting.py change

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8zjwp4t8@qzb#zewpcz+#v@(ph9rprpo%(6^)8su=(v7bqcya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
