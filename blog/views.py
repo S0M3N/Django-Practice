4547215030d2a345a0a5dc393b2ac27914b7ef09
S0M3N <thesomen123@gmail.com>
2022-08-17 21:20:17 +0530

    adding of templates and statics

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def blogIndex(request):
+    return HttpResponse("blog page")
--- home/views.py
+++ home/views.py
@@ -3,13 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    s = """
-    <h1 style='color: green'>Hello World!</h1>
-    <p>lorem ipsum</p>
-    <hr/>
-
-    """
-    return HttpResponse(s)
+    return render(request, 'index.html')
 
 def test(request):
     return HttpResponse("test url")
